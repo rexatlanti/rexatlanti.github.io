476578bb9f104a25657b5877c36b861d0efcd3b8
Fürst <rexatlanti@users.noreply.github.com>
2021-12-01 05:43:36 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hewwo world! &gt;w&lt;</h1>
+      <img src="https://cdn.discordapp.com/emojis/782696325040963605.gif" alt=""></img>
+    </div>
+  );
+}
+
+export default App;
